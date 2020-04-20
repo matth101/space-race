f0d6d6f5afbf53fb8474a86cccd86ee384e2cbfa
matth101 <mhui@ucvts.tec.nj.us>
2020-04-20 09:07:44 -0400

    Remove arrow key scrolling

--- js/sketch.js
+++ js/sketch.js
@@ -30,6 +30,13 @@ window.onload = function() {
     document.getElementById("resetButton").onclick = reset;
 }
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 function preload() {
     spaceship = loadImage('media/spaceship.jpg'); // 35w * 48h  
 }
